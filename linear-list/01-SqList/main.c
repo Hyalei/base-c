fded59b18c8af3e3f1b281a3ca768242294c9efd
haoyalei <hao19903888974@outlook.com>
2025-12-18 16:25:42 +0800

    修改变量命名 添加函数指针示例

--- linear-list/01-SqList/main.c
+++ linear-list/01-SqList/main.c
@@ -6,40 +6,47 @@
 #define LIST_INIT_SIZE 3 //线性表存储空间的初始分配量
 #define LIST_INCREMENT 10 // 线性表存储空间的分配增量
 
-typedef struct SqList {
-    ElemType *elem; // 存储空间基址（数组首地址）
+typedef struct SqList
+{
+    ElemType* elem; // 存储空间基址（数组首地址）
     int length; // 当前长度
     int list_size; // 当前分配的存储容量（以sizeof(ElemType)为单位）
 } SqList;
 
-Status InitList_Sq(SqList *L) {
-    if (L == NULL)
+Status InitList_Sq(SqList* list_ptr)
+{
+    if (list_ptr == NULL)
         return ERROR;
-    L->elem = malloc(LIST_INIT_SIZE * sizeof(ElemType));
-    if (L->elem == NULL)
+    list_ptr->elem = malloc(LIST_INIT_SIZE * sizeof(ElemType));
+    if (list_ptr->elem == NULL)
         return ERROR;
-    L->length = 0;
-    L->list_size = LIST_INIT_SIZE;
+    list_ptr->length = 0;
+    list_ptr->list_size = LIST_INIT_SIZE;
     return OK;
 }
 
-Status ListInsert_Sq(SqList *L, int index, ElemType e) {
+Status ListInsert_Sq(SqList* L, int index, ElemType e)
+{
     if (L == NULL)
         return ERROR;
     // i的合法范围是[1,length+1]，所以i的非法范围是(负无穷, 1)和(length+1,正无穷)
-    if (index < 1 || index > L->length + 1) {
+    if (index < 1 || index > L->length + 1)
+    {
         return ERROR;
     }
     // 当length+1大于数组长度时，扩容数组
-    if (L->length + 1 > L->list_size) {
-        ElemType *newElem = realloc(L->elem, (L->length + LIST_INCREMENT) * sizeof(ElemType));
-        if (newElem == NULL) {
+    if (L->length + 1 > L->list_size)
+    {
+        ElemType* newElem = realloc(L->elem, (L->length + LIST_INCREMENT) * sizeof(ElemType));
+        if (newElem == NULL)
+        {
             exit(OVERFLOW);
         }
         L->elem = newElem;
         L->list_size += LIST_INIT_SIZE;
     }
-    for (int i = L->length; i >= index - 1; i--) {
+    for (int i = L->length; i >= index - 1; i--)
+    {
         L->elem[i + 1] = L->elem[i];
     }
     L->elem[index - 1] = e;
@@ -47,21 +54,25 @@ Status ListInsert_Sq(SqList *L, int index, ElemType e) {
     return OK;
 }
 
-Status ListDelete_Sq(SqList *L, int index, ElemType *e) {
+Status ListDelete_Sq(SqList* L, int index, ElemType* e)
+{
     if (L == NULL)
         return ERROR;
-    if (index < 1 || index > L->length) {
+    if (index < 1 || index > L->length)
+    {
         return ERROR;
     }
     *e = L->elem[index - 1];
-    for (int i = index - 1; i <= L->length - 1; i++) {
+    for (int i = index - 1; i <= L->length - 1; i++)
+    {
         L->elem[i] = L->elem[i + 1];
     }
     L->length--;
     return OK;
 }
 
-Status DestroyList_Sq(SqList *L) {
+Status DestroyList_Sq(SqList* L)
+{
     if (L == NULL)
         return ERROR;
     free(L->elem);
@@ -71,10 +82,12 @@ Status DestroyList_Sq(SqList *L) {
     return OK;
 }
 
-void PrintList(const SqList *L) {
+void PrintList(const SqList* L)
+{
     if (L == NULL)
         return;
-    for (int i = 0; i <= L->length - 1; i++) {
+    for (int i = 0; i <= L->length - 1; i++)
+    {
         printf("%d ", L->elem[i]);
     }
     printf("\n");
@@ -94,7 +107,8 @@ void PrintList(const SqList *L) {
 //     DestroyList_Sq(sq_list_ptr);
 // }
 
-int main(void) {
+int main(void)
+{
     SqList sq_list;
     InitList_Sq(&sq_list);
     ListInsert_Sq(&sq_list, 1, 1);
--- linear-list/02-LinkList/main.c
+++ linear-list/02-LinkList/main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "yans_book_defs.h"
 
-int main(void) {
+typedef struct LNode
+{
+    ElemType data;
+    struct LNode* next;
+} LNode, *LinkList;
+
+Status InitList_Link(LinkList list_ptr)
+{
+    if (list_ptr == NULL)
+        return ERROR;
+    list_ptr->next = NULL;
+    return OK;
+}
+
+int main(void)
+{
+    LNode node;
+    InitList_Link(&node);
     printf("hello world");
 }
+
+// int main(void)
+// {
+//     // 常见错误
+//     // 只声明了一个结构体指针而没有声明结构体，栈中只分配的存储指针的内存，而没有存储结构体的内存。
+//     // 为初始化的指针指向未知内存，导致后续方法通过该指针访问结构体时报错：EXC_BAD_ACCESS
+//     LinkList list_ptr;
+//     InitList_Link(list_ptr);
+//     printf("hello world");
+// }
